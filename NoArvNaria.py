458e777596f18495a96adfbffb3c7ec1dbeb3cca
Arthur Stuhr Miertschink <stuhr.arthur@greenbeltti.onmicrosoft.com>
2022-11-29 16:41:42 -0300

    Documentando o codigo

--- NoArvNaria.py
+++ NoArvNaria.py
@@ -2,131 +2,130 @@ import copy
 
 
 class NoArvNaria:
-    m = 0
-    info = None
-    filhos = [None] * m
-    raiz = None
+    m = 0 #Armazena o numero que indica a quantidade de filhos que a arvore vai ter
+    info = None #Armazena o dado correspondende ao nó
+    filhos = [None] * m #Cria o vetor de filhos com "None" baseado na quantidade de filhos que o usuario deseja
+    raiz = None #Cria a Raiz
 
-    def __init__(self, info=None, m=2):
+    def __init__(self, info=None, m=2): #Responsavel por inicializar a classe
         self.info = info
         self.filhos = [None] * m
 
-    def inicArvnaria(self, m=2):
+    def inicArvnaria(self, m=2): #Metodo responsavel por inicializar uma arvore vazia e retornar a arvore criada, recebe a quantidade de filhos que o usuario deseja que cada arvore tenha (por defult e passado o valor 2)
         return NoArvNaria(None, m)
 
-    def criaArvNaria(self, tipoAN):
+    def criaArvNaria(self, tipoAN): #Metodo responsavel por Criar a arvoreNaria com recebendo o valor que sera a raiz como parametro
         self.info = tipoAN
 
-    def subArvNaria(self):
+    def subArvNaria(self): #Metodo responsavel por retornar as subArvores da arvore
         return self.filhos
 
-    def raizArvoreNaria(self):
-        if self.info is not None:
+    def raizArvoreNaria(self): #Metodo responsavel por retornar a raiz da arvore
+        if self.info is not None: # Verifica se a raiz possui valor
             return self.info
         else:
             return
 
-    def vazioArvNaria(self):
-        if self.info is None and len(self.filhos) == 0:
+    def vazioArvNaria(self): #Metodo responsavel por veriificar se a arvore esta vazia
+        if self.info is None and len(self.filhos) == 0: #verifica se a arvore nao possui raiz nem subarvores
             return True
         else:
             return False
 
-    def insereArvNaria(self, noFilho, noPai):
-        subArvores = self.subArvNaria()
-        if self.info == noPai:
-            if subArvores.count(None) == 0:
+    def insereArvNaria(self, noFilho, noPai): #Metodo responsavel por inserir um no no inicio do array de subArvores
+        subArvores = self.subArvNaria() #Chama o metodo que retorna as subArvores da arvore
+        if self.info == noPai: # Verifica se o pai do no que sera adicionado e a raiz
+            if subArvores.count(None) == 0: # Verifica se o array de subArvores esta cheio
                 return False
-            for f in range(len(subArvores)):
-                index = self.filhos.index(None)
-                if index == 0:
-                    self.filhos[index] = copy.deepcopy(noFilho)
+            for f in range(len(subArvores)): # Faz um loop para percorrer todas as Subarvores
+                index = self.filhos.index(None) # Pega o primeiro indice que esta vazio (None)
+                if index == 0: # Verifica que a subArvore esta completamente vazia
+                    self.filhos[index] = copy.deepcopy(noFilho) # Adiciona o no no array de subArvores da arvore pai
                     return True
                 else:
-                    for i in range(len(subArvores)-1, -1, -1):
-                        self.filhos[i] = copy.deepcopy(self.filhos[i-1])
-                    self.filhos[0] = copy.deepcopy(noFilho)
+                    for i in range(len(subArvores)-1, -1, -1): # Percorre o array de subArvores de tras para frente
+                        self.filhos[i] = copy.deepcopy(self.filhos[i-1]) # Faz uma copia da subArvore de indice anterior para o indice atual, avancando todos em uma posicao
+                    self.filhos[0] = copy.deepcopy(noFilho) #Seta o no que o usuario deseja inserir na primeira posicao do indice de subArvores da arvore
                     return True
         else:
-            for f in self.filhos:
-                if f is None:
+            for f in self.filhos: # Percorre todos as subArvores
+                if f is None: # Verifica se estao vazias
                     continue
-                inserido = f.insereArvNaria(noFilho, noPai)
-                if inserido:
+                inserido = f.insereArvNaria(noFilho, noPai) # Chama novamente o metodo, dessa vez para a subArvore
+                if inserido: #Verifica se o elemento foi inserido com sucesso
                     return True
 
-    def adicionaArvNaria(self, noFilho, noPai):
-        subArvores = self.subArvNaria()
-        if self.info == noPai:
-            if subArvores.count(None) == 0:
+    def adicionaArvNaria(self, noFilho, noPai): # Metodo responsavel por adicionar o no na ultima posicao do array de subArvores da arvore
+        subArvores = self.subArvNaria() # Chama o metodo responsavel por retornar a subArvores
+        if self.info == noPai: # verifica se o no pai e a raiz
+            if subArvores.count(None) == 0: # Verifica se as subArvores estao cheias
                 return False
-            for f in range(len(subArvores) - 1, -1, -1):
-                if self.filhos[f] is None:
-                    self.filhos[f] = copy.deepcopy(noFilho)
+            for f in range(len(subArvores) - 1, -1, -1): #Percorre o array de subArvores de forma inversa
+                if self.filhos[f] is None: # Verifica se a o array de subArvores esta vazio (None)
+                    self.filhos[f] = copy.deepcopy(noFilho) # Insere o no filho na posicao que esta vazia (None)
                     return True
                 else:
-                    for i in range(len(subArvores) - 1):
-                        self.filhos[i] = copy.deepcopy(self.filhos[i+1])
-                    self.filhos[len(subArvores)-1] = copy.deepcopy(noFilho)
-
+                    for i in range(len(subArvores) - 1): # Percorre o array de subArvores
+                        self.filhos[i] = copy.deepcopy(self.filhos[i+1]) # Faz uma copia da subArvore do proximo indice, retardando todos em uma posicao
+                    self.filhos[len(subArvores)-1] = copy.deepcopy(noFilho) # Seta o novo no na ultima posicao do array de subArvores
                     return True
         else:
-            for f in self.filhos:
-                if f is None:
+            for f in self.filhos: # Percorre todas as subArvores
+                if f is None: # Verifica se a subArvore esta vazia (None)
                     continue
-                adicionado = f.adicionaArvNaria(noFilho, noPai)
-                if adicionado:
+                adicionado = f.adicionaArvNaria(noFilho, noPai) # Chama a funcao de inserir novamente, dessa vez para a subArvore
+                if adicionado: # Verifica se a adicao foi um sucesso
                     return True
 
-    def elimSubArvNaria(self, i):
-        filhos = self.subArvNaria()
-        for f in range(len(filhos)):
-            if filhos[f] is None:
+    def elimSubArvNaria(self, i): # Metodo responsavel por eliminar uma subArvore
+        filhos = self.subArvNaria() # Chama o metodo que retorna as subArvores
+        for f in range(len(filhos)): # Verifica todos as subArvores
+            if filhos[f] is None: # Verifica se a subArvore esta vazia (None)
                 continue
-            if filhos[f].info == i:
-                return self.filhos.pop(f)
+            if filhos[f].info == i: # Verifica se o dado que deseja remover está no array de subArvores
+                return self.filhos.pop(f) # Remove o no desejado do array de subArvores
             else:
-                eliminado = filhos[f].elimSubArvNaria(i)
-                if eliminado:
+                eliminado = filhos[f].elimSubArvNaria(i) # Chama o metodo novamente para a subArvore
+                if eliminado: # verifica se foi eliminado com sucesso
                     return eliminado
 
-    def destruirArvNaria(self):
+    def destruirArvNaria(self): # Metodo responsavel por destruir a arvore
         i = 0
-        while i < len(self.filhos):
-            self.filhos.pop()
+        while i < len(self.filhos): # Verifica se o no tem subArvores
+            self.filhos.pop() # Remove as subArvores
 
-        if len(self.filhos) == 0:
+        if len(self.filhos) == 0: # Verifica se nao ha subArvores
             return True
         else:
             return False
 
-    def estaArvNaria(self, info):
-        if self.info == info:
+    def estaArvNaria(self, info): # Metodo responsavel por verificar se um elemento esta presente na arvore
+        if self.info == info: # Verifica se o elemento e a raiz da arvore
             return True
         else:
-            for x in self.filhos:
-                if x is None:
+            for x in self.filhos: # Verifica as subArvores
+                if x is None: # Verifica se a subArvore e vazia (None)
                     continue
-                elif x.info == info:
+                elif x.info == info: # Verifica se a raiz da subArvore e o elemento procurado
                     return True
 
-            for x in self.filhos:
-                if x is None:
+            for x in self.filhos: # Percorre as subArvores
+                if x is None: # Verifica se a subArvore e vazia (None)
                     continue
-                elif x.estaArvNaria(info):
+                elif x.estaArvNaria(info): # Chama o metodo novamente para as subArvores
                     return True
 
             return False
 
-    def imprime(self):
-        print(str(self.info) + " -> ", end="")
-        for x in self.filhos:
-            if x is None:
+    def imprime(self): # Metodo responsavel por imprimir a arvore
+        print(str(self.info) + " -> ", end="") # Imprime a raiz da arvore
+        for x in self.filhos: # Percorre as subArvores
+            if x is None: # Verifica se a subArvore e vazia (None)
                 continue
-            print(str(x.info) + " , ", end="")
+            print(str(x.info) + " , ", end="") # Imprime a raiz das subArvores
         print("")
 
-        for x in self.filhos:
-            if x is None:
+        for x in self.filhos: # Verifica as subArvores
+            if x is None: # Verifica se a subArvore e vazia (None)
                 continue
-            x.imprime()
+            x.imprime() # Chama o metodo novamente para as subArvores
--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from NoArvNaria import NoArvNaria
 if __name__ == '__main__':
 
     print("Bem Vindo ao Simulador de Arvore N-aria")
-    tamanhoFilhos = int(input("Insira o Tamanho Máximo de Filhos em um Nó: "))
-    raiz = input("Insira o Valor a ser a Raíz da Arvore: ")
+    tamanhoFilhos = int(input("Insira o Tamanho Máximo de Filhos em um Nó: ")) #Armazena a quanitdade de nós filhos que o usuário deseja ter na subArvore
+    raiz = input("Insira o Valor a ser a Raíz da Arvore: ") #Armazena o valor da raiz da arvore
     insercoes = input(
         "\nComo Deseja Lidar com Inserções? \n1 - Início da Lista de Filhos \n2 - Fim da Lista de Filhos \nEscolha: ")
-    arvoresDesassociadas = []
-    escolha = -1
+    arvoresDesassociadas = [] #Armazena as subArvores que foram "eliminadas"
+    escolha = -1 #Inicia a variavel escolha
 
-    arvoreNaria = NoArvNaria().inicArvnaria(tamanhoFilhos)
+    arvoreNaria = NoArvNaria().inicArvnaria(tamanhoFilhos) #Inicia a arvoreNaria recebendo como parametro o numero de nós filhos que o usuário deseja
 
-    arvoreNaria.info = raiz
-    arvoreNaria.m = tamanhoFilhos
+    arvoreNaria.info = raiz # seta a raiz da arvore
+    arvoreNaria.m = tamanhoFilhos # seta o tamanho de filhos da arvore
 
     while escolha != 0 and arvoreNaria is not None:
         print("\nEscolha um dos Comandos Abaixo para Prosseguir: ")
@@ -24,40 +24,41 @@ if __name__ == '__main__':
 
         if escolha == "1":
             elem = input("\nDigite o Valor a Ser Inserido na Arvore: ")
-            noFilho = NoArvNaria(elem, tamanhoFilhos)
-            noPai = input("Digite o Nó Pai a ser Associado ao Valor Anterior: ")
-            noFilho.raiz = noPai
-            if (insercoes == "2"):
-                resultado = arvoreNaria.adicionaArvNaria(noFilho, noPai)
+            noFilho = NoArvNaria(elem, tamanhoFilhos) #cria o nó filho
+            noPai = input("Digite o Nó Pai a ser Associado ao Valor Anterior: ") # armazena qual o pai do nó que será inserido
+            noFilho.raiz = noPai # seta o nó pai como saiz do nó filho
+            if (insercoes == "2"): # verifica se o usuário optou por adicionar os nós no final do vetor de filhos
+                resultado = arvoreNaria.adicionaArvNaria(noFilho, noPai) # Adiciona o no filho no final do vetor de filhos do no pai
             else:
-                resultado = arvoreNaria.insereArvNaria(noFilho, noPai)
-            if resultado:
+                resultado = arvoreNaria.insereArvNaria(noFilho, noPai) # Adiciona o no filho no inicio do vetor de filhos do no pai
+            if resultado: # Verifica se a insercao deu certo
                 print(elem, "incluído com sucesso")
             else:
                 print("\nSubarvore Pai Já Está Cheia ou Nó Pai Inválido")
         elif escolha == "2":
-            no = input("\nDigite a Subarvore que Deseja Remover: ")
-            if arvoreNaria.info == no:
-                arvoreNaria = None
-                eliminado = True
+            no = input("\nDigite a Subarvore que Deseja Remover: ") #Armazena o valor do nó da subArvore que o usuario quer remover
+            if arvoreNaria.info == no: #Verifica se o no e a raiz
+                arvoreNaria = None #Seta a arvore como None
+                eliminado = True #Seta a variavel eliminado como true
             else:
-                eliminado = arvoreNaria.elimSubArvNaria(no)
-                arvoresDesassociadas.append(eliminado)
-            if eliminado:
+                eliminado = arvoreNaria.elimSubArvNaria(no) #Chama o metodo responsavel por eliminar a subArvore passando o no desejado como parametro
+                arvoresDesassociadas.append(eliminado) #Inclui a subArvore que foi eliminada no vetor que armazena subArvores "eliminadas"
+            if eliminado: # Verifica se a subArvore foi eliminada com sucesso
                 print("\nNo removido com sucesso")
-                if arvoreNaria is None:
+                if arvoreNaria is None: #Verifica se a arvore esta vazia
                     print("\nArvore Naria Destruída.\nEncerrando o Programa...")
+                    break
         elif escolha == "3":
             print("Arvore Naria Destruída.\nEncerrando o Programa...")
-            arvoreNaria.destruirArvNaria()
+            arvoreNaria.destruirArvNaria() #Chama o metodo responsavel por destruir a arvore
             break
         elif escolha == "4":
-            arvoreNaria.imprime()
+            arvoreNaria.imprime() #Chama o metodo responsavel por imprimir a arvore
         elif escolha == "5":
-            no = input("\nDigite o Nó a ser Verificado: ")
-            busca = arvoreNaria.estaArvNaria(no)
+            no = input("\nDigite o Nó a ser Verificado: ") #Armazena o valor do no que o usuario deseja pesquisar
+            busca = arvoreNaria.estaArvNaria(no) #Chama a funcao que verifica se o elemento esta na arvore passando o valor do no que o usuario deseja buscar como parametro
 
-            if busca:
+            if busca: #Verifica se o no foi encontrado
                 print("Elemento Está Contino na Árvore")
             else:
                 print("Elemento Não Está Contino na Árvore")
